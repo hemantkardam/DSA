f5ecb967e604036eac9f9e61dbae1d5a9c272d76
hemantkardam <hemantkardam8287@gmail.com>
2024-07-23 14:47:01 +0530

    update

---
+++ Table.cpp
@@ -0,0 +1,36 @@
+// srand()
+// rand()
+// cstdlib()
+// ctime()
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    // int n;
+    // cout<<"Enter a number for print the table:-";
+    // cin>>n;
+
+    // int i=1;
+    // do
+    // {
+    //     cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+    //     i++;
+    // } while (i<=10);
+    
+
+    // int n;
+    // cout<<"Enter a number for sum of all the number :-";
+    // cin>>n;
+    // int i=1,sum=0;
+    // do
+    // {
+    //     cout<<sum<<" + "<<i<<" = ";
+    //     sum=i+sum;
+    //     cout<<sum<<endl;
+    //     i++;
+    // } while (i<=n);
+    
+}
