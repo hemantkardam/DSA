8024a2f1fda97f62bea5e005c37439107b5a895f
hemantkardam <hemantkardam8287@gmail.com>
2024-07-24 15:15:24 +0530

    Update

--- 123Pattern.cpp
+++ 123Pattern.cpp
@@ -13,13 +13,13 @@ cin>>n;
 // }
 // cout<<endl;
 // }
-for(int i=1;i<=n;i++)
-{
-for(int j=1;j<=i;j++)
-{
-    cout<<i<<" ";
-}
-cout<<endl;
-}
+// for(int i=1;i<=n;i++)
+// {
+// for(int j=1;j<=i;j++)
+// {
+//     cout<<i<<" ";
+// }
+// cout<<endl;
+// }
 
 }
