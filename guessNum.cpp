ce741cf52bfaae020b9d4bfd9f1475c842c18346
hemantkardam <hemantkardam8287@gmail.com>
2024-07-25 20:39:26 +0530

    Update

---
+++ guessNum.cpp
@@ -0,0 +1,19 @@
+// srand()
+// rand()
+// cstdlib()
+// ctime()
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+#include<cstdlib>
+#include<ctime>
+
+
+int main(){
+    int rN;
+    rN=rand();
+
+    std::cout<<rN;
+}
