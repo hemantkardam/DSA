8b9360ef58bad0dbb1d13bf5ffd237d4f0d4d1f6
hemantkardam <hemantkardam8287@gmail.com>
2024-07-25 20:34:11 +0530

    Update

---
+++ oddEven.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    if(n%2==0){
+        cout<<"Even number.";
+    }
+    else if(n%2!=0)
+        cout<<"odd number.";
+
+}
