4dd2caaeec03e7c52b00838f5843b319ce86e225
hemantkardam <hemantkardam8287@gmail.com>
2024-07-25 20:33:21 +0530

    Update

---
+++ calculator.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char a;
+    int m,n;
+
+    cout<<"Enter a two number: ";
+    cin>>m>>n;
+    cout<<"Enter the first letter of operation.";
+    cin>>a;
+
+    switch (a)
+    {
+    case 'A':cout<<m+n;
+        break;
+    case 'S':cout<<m-n;
+        break;
+    case 'D':cout<<m/n;
+        break;
+    case 'M':cout<<m*n;
+        break;
+    default:cout<<"Invalid choice! Pls try again.";;
+        break;
+    }
+}
