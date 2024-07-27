2077b65d9a87856ce4db09914630173b3f8f7bac
hemantkardam <hemantkardam8287@gmail.com>
2024-07-27 13:54:43 +0530

    Update

---
+++ Pattern/SqStar.cpp
@@ -0,0 +1,44 @@
+// -------------- For a Square star pattern
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number :";
+    cin>>n;
+// ------------By For Loop ---------------
+    // for(int i=1;i<=n;i++)
+    // {
+    //     for(int j=1;j<=n;j++){
+    //         if(j==1 || j==n || i==1 || i==n){
+    //             cout<<"* ";
+    //         }
+    //         else{
+    //             cout<<"  ";
+    //         }
+    // }
+    //     cout<<endl;
+    // }
+
+
+// -------------------By While Loop ----------------------
+    
+    // int i=1;
+    // while(i<=n)
+    // {
+    //     int j=1;
+    //     while(j<=n)
+    //     {
+    //         if(i==1 || i==n || j==1 || j==n){
+    //             cout<<"* "; 
+    //         }
+    //         else{
+    //             cout<<"  ";
+    //         }
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+}
