e0f8ce0b8409e8f87d91199171b207068873fc8d
hemantkardam <hemantkardam8287@gmail.com>
2024-07-27 13:54:21 +0530

    Update

---
+++ Pattern/Pyramid.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+//-----------For a Pyramid -------------------------
+    
+    // int i=1;
+    // while(i<=n){
+    //     int k=1;
+    //     while(k<=n-i){
+    //         cout<<"  ";
+    //         k++;
+    //     }
+    //     int j=1;
+    //     while(j<=i*2-1){
+    //         cout<<"* ";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+// --------- For a Opposite pyramid ----------
+
+
+    // int i=n;
+    // while(i>=1)
+    // {
+    //     int k=1;
+    //     while(k<=n-i){
+    //         cout<<"  ";
+    //         k++;
+    //     }
+    //     int j=1;
+    //     while(j<=2*i-1)
+    //     {
+    //         cout<<"* ";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i--;
+    // }
+}
