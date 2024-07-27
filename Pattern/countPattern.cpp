bd5ab0cb2281a17d7b70d9bf732db1b59ef37f93
hemantkardam <hemantkardam8287@gmail.com>
2024-07-27 13:53:19 +0530

    Update

---
+++ Pattern/countPattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    int i=1,count=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<count<<" ";
+            count++;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+}
