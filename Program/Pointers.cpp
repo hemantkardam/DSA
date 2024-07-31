8c667c21fdfe65e6eed755c1ad32c71cdc9d001a
hemantkardam <hemantkardam8287@gmail.com>
2024-07-31 14:57:51 +0530

    Update

--- Program/Pointers.cpp
+++ Program/Pointers.cpp
@@ -1,9 +1,57 @@
+//  Type of Pointers ->
+// 1. Int pointer 
+// 2. Double pointer -> Data type
+// 3. Float pointer 
+// 4. Char pointer 
+// 5. Long pointer 
+
+// 7. NUll pointer 
+// 8. Double pointer  (**) 
+// 9. void pointer 
+// 10. nullptr 
+// 11. Dangling Pointer 
+
+// 12. Arithmetic Pointer 
+// 13. Smart Pointers
+        // a. Shared ptr
+        // b. Unique ptr
+        // c. Weak ptr
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    int b=10;
-    int z=20;
-   int  *a=&z;
-    cout<<&a;
+    
+    // int b=10;
+    // int  *a=&b;
+    
+    // cout<<b <<endl;
+    // cout<<&b <<endl;
+    // cout<<&a<<endl;
+
+    //It is called Derefrencing cout<<*a<<endl;
+    // cout<<a<<endl;
+
+    //Same Data type variable create for pointer
+    
+    // float z=20;
+    // float *c=&z;
+
+    // cout<<z<<endl;
+    // cout<<*c<<endl;
+    // cout<<&z<<endl;
+    // cout<<c<<endl;
+    // cout<<&c<<endl;
+
+    //char datatype is consider address as a char so print A💖a. So i used (void*) for overriding it.
+    
+    // char cd='A';
+    // char *ch=&cd;
+
+    // cout<<cd<<endl;
+    // cout<<*ch<<endl;
+    // cout<<(void*)ch<<endl;
+    // cout<<(void*)&cd<<endl;
+    // cout<<&ch<<endl;
+    
 }
