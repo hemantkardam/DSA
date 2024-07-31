8205e532291a127dc78affac0b85e1072c766e07
hemantkardam <hemantkardam8287@gmail.com>
2024-07-31 15:16:08 +0530

    Update

---
+++ Program/size.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<endl;
+    cout<<"Size of char :"<<sizeof( char)<<endl;
+    cout<<endl;
+    cout<<"Size of short int :"<<sizeof(short int)<<endl;
+    cout<<"Size of int :"<<sizeof( int)<<endl;
+    cout<<"Size of signed int :"<<sizeof( signed int)<<endl;
+    cout<<"Size of unsigned int :"<<sizeof( unsigned int)<<endl;
+    cout<<endl;
+    cout<<"Size of float :"<<sizeof( float)<<endl;
+    cout<<"Size of double :"<<sizeof( double)<<endl;
+    cout<<"Size of long double :"<<sizeof( long double)<<endl;
+    cout<<endl;
+    cout<<"Size of string :"<<sizeof(string)<<endl;
+    cout<<endl;
+}
