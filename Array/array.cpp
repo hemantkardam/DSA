97a8ba053c41b09ca44ddea31d5fb0ed0e9ab993
hemantkardam <hemantkardam8287@gmail.com>
2024-08-06 17:56:59 +0530

    Update

---
+++ Array/array.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int a[10]={1,2,3,4,5,6,7,8,9,10};
+    // for(int i=0;i<10;i++)
+    // cout<<a[i]<<"  ";
+
+    // cout<<endl<<&a<<endl;
+    // cout<<a<<endl;
+    // cout<<*a<<endl;
+
+
+    int *arr=new int[10];
+
+    for(int i=0;i<10;i++){
+        cin>>arr[i];
+    }    
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }    
+    cout<<endl<<&arr<<endl;
+    cout<<arr<<endl;
+    cout<<endl<<&arr<<endl;
+    
+    // for address
+    cout<<arr<<endl;
+    cout<<arr+1<<endl;
+    cout<<arr+2<<endl;
+
+    // for Value
+    cout<<*arr<<endl;
+    cout<<*(arr+1)<<endl;
+    cout<<*(arr+2)<<endl;
+    cout<<*(arr+3)<<endl;
+}
