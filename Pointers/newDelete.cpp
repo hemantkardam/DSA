b6ce66403b33bf8157185e7ddb392680a0a54bc3
hemantkardam <hemantkardam8287@gmail.com>
2024-08-13 17:31:30 +0530

    Update

--- Pointers/newDelete.cpp
+++ Pointers/newDelete.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    char *c=new char('H');
+    char *ch=new char('H');
     int *p=new int(5);
     float *d=new float(7.23);
     cout<<p<<endl;
@@ -10,7 +10,7 @@ int main(){
     cout<<d<<endl;
     cout<<d+1<<endl;
     cout<<*d<<endl;
-    cout<<(void*)c<<endl;
-    cout<<(void*)c+1<<endl;
+    cout<<(void*)ch<<endl;
+    cout<<(void*)ch<<endl;
     // return 0;
 }
