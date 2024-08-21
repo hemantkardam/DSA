9fa8f1a14f4924952bd7e94a40da7620d59275c2
Hemant Kardam <hemantkardam8287@gmail.com>
2024-08-21 22:07:02 +0530

    Update

---
+++ Operator/prefrence.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<8*9-12/3+40<<endl;
+    cout<<3*2-11*4+60<<endl;
+    cout<<80-20/2+3*6<<endl;
+    cout<<90/3*2-60+2<<endl;
+    cout<<7*8/4+80-90<<endl;
+}
