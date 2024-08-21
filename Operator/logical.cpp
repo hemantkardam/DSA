0b3f54d56bd88788363b732b726795f19d179bc2
Hemant Kardam <hemantkardam8287@gmail.com>
2024-08-21 22:06:46 +0530

    Update

---
+++ Operator/logical.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=0&&0;
+    cout<<a;
+    cout<<endl;
+
+     a=0&&1;
+    cout<<a;
+    cout<<endl;
+
+     a=1&&0;
+    cout<<a;
+    cout<<endl;
+
+     a=1&&1;
+    cout<<a;
+    cout<<endl;
+
+     a=0||0;
+    cout<<a;
+    cout<<endl;
+
+     a=1||0;
+    cout<<a;
+    cout<<endl;
+
+     a=0||1;
+    cout<<a;
+    cout<<endl;
+
+    a=1||1;
+    cout<<a;
+    cout<<endl;
+} 
