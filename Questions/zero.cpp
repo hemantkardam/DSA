030239acbcd256ae8f313c8a4a2aa121bf2eb0f1
Hemant Kardam <hemantkardam8287@gmail.com>
2024-09-03 20:14:40 +0530

    update

---
+++ Questions/zero.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void sort(int *a,int n){
+    int count=0;
+    for(int i=0;i<10;i++){
+        if(a[i]!=0){
+            swap(a[i],a[count]);
+            count++;
+        }
+    }
+}
+
+int main(){
+    int a[10]={0,5,0,0,0,10,5,0,8,7};
+    int n=sizeof(a)/sizeof(a[0]);
+    sort(a,n);    
+    for(int i=0;i<10;i++){
+        cout<<a[i]<<"\t";
+    }
+}
