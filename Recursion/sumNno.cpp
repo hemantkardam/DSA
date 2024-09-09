624e2e9cc673ea383bd3cbb1e31e8c3cbfd06e31
Hemant Kardam <hemantkardam8287@gmail.com>
2024-09-09 15:35:04 +0530

    update

---
+++ Recursion/sumNno.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+// int print(int n,int k){
+//     if(n==0){
+//         return;
+//     }
+//     cout<<k<<" ";
+//     print(n-1,k+1);
+// }
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     // int k=1;
+//     // cout<<
+//     print(n,1);
+
+//     return 0;
+// }
+
+int func(int n){
+    if(n==0){
+        return 0;
+    }
+    return func(n/2)+func(n/4);
+}
+
+int main(){
+    cout<<func(5);
+
+    return 0;
+}
