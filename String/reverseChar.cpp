6d25ae660dbcff30bd2b8d00014a0ca26e84d539
Hemant Kardam <hemantkardam8287@gmail.com>
2024-09-17 15:51:09 +0530

    update

--- String/reverseChar.cpp
+++ String/reverseChar.cpp
@@ -32,3 +32,25 @@ int main(){
     reverse(a,n);
     print(a,n);
 }
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+void swapStr(string str,int n){ 
+    int l=0;
+    int h=n-1;
+    while(l<=h){
+        swap(str[l],str[h]);
+        l++;
+        h--;
+    }
+    cout<<str<<endl;
+}
+int main(){
+    string str;
+    getline(cin,str);
+    int n=str.size();
+    // cout<<n;
+    swapStr(str,n);
+}
