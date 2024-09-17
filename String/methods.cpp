fa54f339fdc1de29dfccdd73e0e8e40d6113af58
Hemant Kardam <hemantkardam8287@gmail.com>
2024-09-17 15:40:49 +0530

    update

--- String/methods.cpp
+++ String/methods.cpp
@@ -1,21 +1,33 @@
 #include<iostream>
 #include<string>
+#include<typeinfo>
 using namespace std;
 
-void swapStr(string str,int n){ 
-    int l=0;
-    int h=n-1;
-    while(l<=h){
-        swap(str[l],str[h]);
-        l++;
-        h--;
-    }
-    cout<<str<<endl;
-}
-
 int main(){
-    string str = "c++";
-    int n=sizeof(str);
-    cout<<n;
-    swapStr(str,n);
+    int test=5;
+    string n="harrykardam";
+    string space=" ";
+    string number="1";
+    string s="kardam";
+    // cout<<"Length of string :"<<s.length()<<endl; // length of the string.
+    // cout<<"Size of string :"<<s.size()<<endl; // size of the string.
+    // cout<<n.insert(5,space)<<endl; // to insert a string into a another string.
+    // cout<<n.append(s)<<endl; // to add a two string.
+    // cout<<s.back()<<endl; // to print last letter of string.
+    // cout<<s.front()<<endl; // to print first letter of string.
+    // cout<<s.at(2); // to print which value stored in particular indexing.
+    // int a=stoi(number); // to convert string to integer(s to i).
+    // cout<<typeid(a).name(); // to print datatype in code (int = i,float = f, string = NSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE).
+    // string convert = to_string(test); // convert into string.
+    // n.push_back('l'); // for push a character in string.
+    // cout<<n<<endl;
+    // n.pop_back();
+    // cout<<n<<endl; // for pop a last character in string.
+    
+    // cout<<a+1<<endl;
+    // cout<<convert<<endl;
+    // cout<<typeid(convert).name();
+
+
+    return 0;
 }
