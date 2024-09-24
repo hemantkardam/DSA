d01c89bd1c69ef979e0c7cf416c2e13c7e182a8b
Hemant Kardam <hemantkardam8287@gmail.com>
2024-09-24 19:51:48 +0530

    update

--- Practice/HollowSquare.cpp
+++ Practice/HollowSquare.cpp
@@ -2,36 +2,36 @@
 using namespace std;
 
 int main(){
-    // int r,c;
-    // cout<<"Enter a Row no :";
-    // cin>>r;
-    // cout<<"Enter a Column no :";
-    // cin>>c;
+    int r,c;
+    cout<<"Enter a Row no :";
+    cin>>r;
+    cout<<"Enter a Column no :";
+    cin>>c;
 
-    // for(int i=1;i<=r;i++){
-    //     for (int j=1; j<=c; j++)
-    //     {
-    //         if(i==1 || i==r || j==1 || j==c)
-    //         cout<<"* ";
-    //         else{
-    //             cout<<"  ";
-    //         }
-    //     }
-    //     cout<<endl;
-    // }
-    int n;
-    cout<<"Enter a number :";
-    cin>>n;
-    for(int i=n;i>=1;i--){
-        for (int k=i-1; k>=1; k--)
+    for(int i=1;i<=r;i++){
+        for (int j=1; j<=c; j++)
         {
-            cout<<"  ";
-        }
-        for (int j=n-i+1; j>=1; j--)
-        {
+            if(i==1 || i==r || j==1 || j==c)
             cout<<"* ";
+            else{
+                cout<<"  ";
+            }
         }
         cout<<endl;
     }
+    // int n;
+    // cout<<"Enter a number :";
+    // cin>>n;
+    // for(int i=n;i>=1;i--){
+    //     for (int k=i-1; k>=1; k--)
+    //     {
+    //         cout<<"  ";
+    //     }
+    //     for (int j=n-i+1; j>=1; j--)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
     return 0;
 }
