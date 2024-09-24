465daaba0cfadb5bfd4519e8cb9144713dbcc896
Hemant Kardam <hemantkardam8287@gmail.com>
2024-09-24 19:51:43 +0530

    update

---
+++ Assignment/String/q3.cpp
@@ -0,0 +1,26 @@
+// Objective: Write a C++ program to convert all lowercase characters in a string to uppercase.
+// Input:
+// • A single string s of length n where 1 ≤ n ≤ 1000. The string may contain a mix of uppercase, 
+// lowercase, and non-alphabetic characters.
+// Output:
+// • The string with all lowercase letters converted to uppercase.
+// Example 1:
+// Input:
+// Hello World
+// Output:
+// HELLO WORLD
+
+#include<iostream>
+#include<string>
+#include<cctype>
+
+using namespace std;
+
+int main(){
+    string str;
+    getline(cin,str);
+    cout<<str<<endl;
+    cout<<str<<endl;
+
+    
+}
