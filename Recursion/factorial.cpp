cc87b7b304cd92b62149bd2db3e3eae6f6417842
Hemant Kardam <hemantkardam8287@gmail.com>
2024-09-28 14:53:09 +0530

    update

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int fac(int a){
+    if(a==0){
+        return 1;
+    }
+    // cout<<a<<" ";
+    return a*fac(a-1);
+}
+
+int main(){
+    int a;
+    cin>>a;
+    cout<<fac(a);
+
+    return 0;
+}
