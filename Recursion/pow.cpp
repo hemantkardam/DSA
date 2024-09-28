578e015afdbdf1847d4719dd393bbaddff8cf724
Hemant Kardam <hemantkardam8287@gmail.com>
2024-09-28 14:53:16 +0530

    update

---
+++ Recursion/pow.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int pow(int a,int p,int count){
+    // int count=1;
+    
+
+
+}
+
+int main(){
+    int a,p;
+    int count=1;
+    cout<<"Enter a number :";
+    cin>>a;
+    cout<<"Enter the power :";
+    cin>>p;
+    pow(a,p,count);
+
+    return 0;
+}
