7ca18052c1570028a5319ff645bae1a39f225997
Hemant Kardam <hemantkardam8287@gmail.com>
2024-10-05 21:37:59 +0530

    update

--- Pointers/voidPointer.cpp
+++ Pointers/voidPointer.cpp
@@ -9,14 +9,12 @@ int main(){
     void *s=&a;
     void *d=&b;
     void *e=&c;
-
-
     cout<<s<<endl;
     cout<<d<<endl;
     cout<<e<<endl;
     cout<<endl;
     
-    
+
     // error: 'void*' is not a pointer-to-object type
     // cout<<*s<<endl;
     // cout<<*d<<endl;
